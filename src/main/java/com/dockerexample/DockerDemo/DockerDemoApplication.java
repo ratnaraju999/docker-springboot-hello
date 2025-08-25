f3f37629e378d54461603f8386457221c7a189cf
ratnaraju999 <ratnaraj111@gmail.com>
2025-08-25 20:35:33 +0530

    Added POST endpoint

--- src/main/java/com/dockerexample/DockerDemo/DockerDemoApplication.java
+++ src/main/java/com/dockerexample/DockerDemo/DockerDemoApplication.java
@@ -1,9 +1,13 @@
 package com.dockerexample.DockerDemo;
 
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -22,6 +26,11 @@ public class DockerDemoApplication {
 	 public String helloName(@PathVariable String name) {
 	     return "Hello, " + name + "!";
 	 }
+	 @PostMapping("/greet")
+	 public String greet(@RequestBody Map<String, String> payload) {
+	     return "Hello, " + payload.get("name") + "!";
+	 }
+
 
 
 }
